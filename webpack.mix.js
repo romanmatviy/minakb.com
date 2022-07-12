0de2b83049b72716e3540d01ea28fc5659057f74
RomanMatviy <matviy.pp.ua@gmail.com>
2022-07-12 18:36:45 +0300

    change on laravel.mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,6 +6,7 @@ mix
 .copy('style/scss/images/*.*', 'style/images')
 
 .sass('style/scss/login.scss', 'style')
+.sass('style/scss/profile.scss', 'style')
 .sass('style/scss/comments.scss', 'style')
 .sass('style/scss/ws__main.scss', 'style')
 .sass('style/scss/style.scss', 'style').browserSync('http://ministerstvo.localhost/');
